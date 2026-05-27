1ff4618f1bf1bdc2f02ad2ba61197d55dd642e8d
shaiksaidhul <157446745+shaiksaidhul@users.noreply.github.com>
2026-05-27 07:37:06 +0530

    added

---
+++ api/create-order.js
@@ -0,0 +1,34 @@
+import Razorpay from "razorpay";
+
+export default async function handler(req, res) {
+
+  if (req.method !== "POST") {
+    return res.status(405).json({
+      message: "Method not allowed"
+    });
+  }
+
+  try {
+
+    const razorpay = new Razorpay({
+      key_id: process.env.RAZORPAY_KEY_ID,
+      key_secret: process.env.RAZORPAY_KEY_SECRET,
+    });
+
+    const options = {
+      amount: 12000 * 100,
+      currency: "INR",
+      receipt: "skillvane_receipt",
+    };
+
+    const order = await razorpay.orders.create(options);
+
+    res.status(200).json(order);
+
+  } catch (error) {
+
+    res.status(500).json({
+      error: error.message,
+    });
+  }
+}
